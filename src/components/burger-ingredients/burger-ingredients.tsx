858fb1be6788796c2c0716b057c002ff3449e962
rdmitriev78 <72352383+rdmitriev78@users.noreply.github.com>
2026-09-19 13:25:10 +0300

    first commit

---
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -0,0 +1,51 @@
+import { Tab } from '@krgaa/react-developer-burger-ui-components';
+
+import type { TIngredient } from '@utils/types';
+
+import styles from './burger-ingredients.module.css';
+
+type TBurgerIngredientsProps = {
+  ingredients: TIngredient[];
+};
+
+export const BurgerIngredients = ({
+  ingredients,
+}: TBurgerIngredientsProps): React.JSX.Element => {
+  console.log(ingredients);
+
+  return (
+    <section className={styles.burger_ingredients}>
+      <nav>
+        <ul className={styles.menu}>
+          <Tab
+            value="bun"
+            active={true}
+            onClick={() => {
+              /* TODO */
+            }}
+          >
+            Булки
+          </Tab>
+          <Tab
+            value="main"
+            active={false}
+            onClick={() => {
+              /* TODO */
+            }}
+          >
+            Начинки
+          </Tab>
+          <Tab
+            value="sauce"
+            active={false}
+            onClick={() => {
+              /* TODO */
+            }}
+          >
+            Соусы
+          </Tab>
+        </ul>
+      </nav>
+    </section>
+  );
+};
